63a1d258e40ba9ca838819abf2b6839c38b1c94f
Aio <AioDev.JS@gmail.com>
2021-05-19 01:11:31 +0900

    #2 메인 화면 포스팅 무한 스크롤

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import useSWR from 'swr';
+import useSWR, { useSWRInfinite } from 'swr';
 import Head from 'next/head';
 import { Col, Row } from 'antd';
 import AppLayout from '../components/layouts/AppLayout';
@@ -9,20 +9,44 @@ import { generateDummyPost } from '../util/dummy';
 import MainPostCard from '../components/MainPostCard';
 
 // const fetcher = (url) => axios.get(url, { withCredentials: true }).then((result) => result.data);
-const fakeFecther = (url) => Promise.resolve(generateDummyPost(10));
+const fakeFecther = (url) => Promise.resolve(generateDummyPost(12));
+
+const getKey = (pageIndex, previousPageData) => {
+  if (previousPageData && !previousPageData.length) return null; // reached the end
+  return `/posts?page=${pageIndex}&limit=12`; // SWR key
+};
 
 const Home = () => {
   const [postsLoadLimit, setPostsLoadLimit] = useState(10);
 
-  const { data: postsData, error: postsError } = useSWR(
-    `${backUrl}/user/followers?limit=${postsLoadLimit}`,
-    fakeFecther,
-  );
+  const {
+    data: postsDataArray,
+    error: postsError,
+    size,
+    setSize,
+  } = useSWRInfinite(getKey, fakeFecther);
 
-  if (!postsData) {
+  const hasMorePosts = true;
+  useEffect(() => {
+    function onScroll() {
+      if (
+        window.pageYOffset + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 100
+      ) {
+        if (postsDataArray && postsDataArray.length === size) {
+          setSize(size + 1);
+        }
+      }
+    }
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [postsDataArray]);
+
+  if (!postsDataArray) {
     return <div>로딩중</div>;
   }
-  console.log(postsData[0]);
 
   return (
     <>
@@ -31,9 +55,9 @@ const Home = () => {
       </Head>
       <AppLayout>
         <Row gutter={8} align="middle">
-          {postsData.map((post) => (
-            <MainPostCard post={post} />
-          ))}
+          {postsDataArray.map((arr) =>
+            arr.map((post) => <MainPostCard post={post} />),
+          )}
         </Row>
       </AppLayout>
     </>
