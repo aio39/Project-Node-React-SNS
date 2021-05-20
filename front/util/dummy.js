091c2bd212aa2441c83fc7abdaedbd10b76f38e6
Aio <AioDev.JS@gmail.com>
2021-05-20 23:21:42 +0900

    #2 dummy 업데이트

--- front/util/dummy.js
+++ front/util/dummy.js
@@ -1,31 +1,59 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable import/prefer-default-export */
 import shortId from 'shortid';
 import faker from 'faker';
 
-export const generateDummyPost = (number) =>
+export const generateDummyUser = () => ({
+  id: shortId.generate(),
+  nickname: faker.name.findName(),
+  email: faker.internet.email(),
+  description: faker.lorem.text(200),
+  avatar: 'https://source.unsplash.com/user/avatar/100x100',
+});
+
+export const generateDummyComment = () => ({
+  id: shortId.generate(),
+  User: generateDummyUser(),
+  content: faker.lorem.sentence(),
+  createAt: faker.date.recent(),
+  sub:
+    Math.random() > 0.5
+      ? [generateDummyComment(), generateDummyComment()]
+      : null,
+});
+
+export const generateDummyPostImage = () => ({
+  id: shortId.generate(),
+  title: faker.lorem.word(10),
+  src: 'https://source.unsplash.com/user/erondu/1600x900',
+});
+
+export const generateDummyTags = (number) =>
   Array(number)
     .fill()
     .map(() => ({
       id: shortId.generate(),
-      User: {
-        id: shortId.generate(),
-        nickname: faker.name.findName(),
-        avatar: 'https://source.unsplash.com/user/avatar/100x100',
-      },
-      title: faker.lorem.word(Math.random() * 30 - 10),
-      content: faker.lorem.paragraph(),
-      Images: [
-        {
-          src: 'https://source.unsplash.com/user/erondu/1600x900',
-        },
-      ],
-      Comments: [
-        {
-          User: {
-            id: shortId.generate(),
-            nickname: faker.name.findName(),
-          },
-          content: faker.lorem.sentence(),
-        },
-      ],
+      name: faker.lorem.words(1),
     }));
+
+export const generateDummyPost = () => ({
+  id: shortId.generate(),
+  User: generateDummyUser(),
+  title: faker.lorem.word(Math.random() * 30 - 10),
+  content: faker.lorem.paragraph(50),
+  Images: Array(parseInt(Math.random() * 4 + 1, 10))
+    .fill()
+    .map(() => generateDummyPostImage()),
+  Tag: generateDummyTags(3),
+  Comments: generateDummyComments(3),
+});
+
+export const generateDummyComments = (number) =>
+  Array(number)
+    .fill()
+    .map(() => generateDummyComment());
+
+export const generateDummyPosts = (number) =>
+  Array(number)
+    .fill()
+    .map(() => generateDummyPost());
