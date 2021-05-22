a3c1fe347bf66fe4202f6c29c8971d3b54c35b36
Aio <AioDev.JS@gmail.com>
2021-05-22 21:59:53 +0900

    #2 메인 포스트카드에 링크 추가

--- front/components/MainPostCard.js
+++ front/components/MainPostCard.js
@@ -5,35 +5,39 @@ import Meta from 'antd/lib/card/Meta';
 import Text from 'antd/lib/typography/Text';
 import Title from 'antd/lib/typography/Title';
 import React from 'react';
+import Link from 'next/link';
 
 const MainPostCard = ({ post }) => {
   console.log(post);
   return (
     <>
       <Col style={{ backgroundColor: '#f39' }} xs={24} md={12} xl={6}>
-        <Card
-          title="card title"
-          hoverable
-          style={{ width: '100%' }}
-          cover={<img alt={post.title} src={post.Images[0].src} />}
-        >
-          <Meta
-            avatar={<Avatar src={post.User.avatar} />}
-            title={<Title level={3}>{post.title}</Title>}
-            description={
-              <Text>
-                {post.content.length > 150
-                  ? `${post.content.slice(0, 150)}...`
-                  : post.content}
-              </Text>
-            }
-          />
-          <Divider orientation="left">Tags</Divider>
-          {post.Tag.map((tag) => (
-            <Tag> {tag.name}</Tag>
-          ))}
-        </Card>
-        ,
+        <Link href={`/post/${post.id}`}>
+          <Card
+            title={`${post.title}`}
+            hoverable
+            style={{ width: '100%' }}
+            cover={<img alt={post.title} src={post.Images[0].src} />}
+          >
+            <Meta
+              avatar={<Avatar src={post.User.avatar} />}
+              title={<Title level={3}>{post.title}</Title>}
+              description={
+                <Text>
+                  {post.content.length > 150
+                    ? `${post.content.slice(0, 150)}...`
+                    : post.content}
+                </Text>
+              }
+            />
+            <Divider orientation="left">Tags</Divider>
+            {post.Tag.map((tag) => (
+              <Link href={`/tag/${tag.name}`}>
+                <Tag> {tag.name}</Tag>
+              </Link>
+            ))}
+          </Card>
+        </Link>
       </Col>
     </>
   );
