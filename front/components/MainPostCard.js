3b59b88dadffffc3f877a0fbb708f0d3e6d7e96e
Aio <AioDev.JS@gmail.com>
2021-05-27 20:33:59 +0900

    #2 메인 포스트 카드, undefind 핸들링

--- front/components/MainPostCard.js
+++ front/components/MainPostCard.js
@@ -7,40 +7,52 @@ import Title from 'antd/lib/typography/Title';
 import React from 'react';
 import Link from 'next/link';
 
-const MainPostCard = ({ post }) => {
-  console.log(post);
-  return (
-    <>
-      <Col style={{ backgroundColor: '#f39' }} xs={24} md={12} xl={6}>
-        <Link href={`/post/${post.id}`}>
-          <Card
-            title={`${post.title}`}
-            hoverable
-            style={{ width: '100%' }}
-            cover={<img alt={post.title} src={post.Images[0].src} />}
-          >
-            <Meta
-              avatar={<Avatar src={post.User.avatar} />}
-              title={<Title level={3}>{post.title}</Title>}
-              description={
-                <Text>
-                  {post.content.length > 150
-                    ? `${post.content.slice(0, 150)}...`
-                    : post.content}
-                </Text>
+const MainPostCard = ({ post }) => (
+  <>
+    <Col style={{ backgroundColor: '#f39' }} xs={24} md={12} xl={6}>
+      <Link href={`/post/${post.id}`}>
+        <Card
+          title={`${post.title}`}
+          hoverable
+          style={{ width: '100%' }}
+          cover={
+            <img
+              alt={post.title}
+              src={
+                post.Images[0]?.src ||
+                'https://source.unsplash.com/user/avatar/100x100'
               }
             />
-            <Divider orientation="left">Tags</Divider>
-            {post.Tag.map((tag) => (
-              <Link href={`/tag/${tag.name}`}>
-                <Tag> {tag.name}</Tag>
-              </Link>
-            ))}
-          </Card>
-        </Link>
-      </Col>
-    </>
-  );
-};
+          }
+        >
+          <Meta
+            avatar={
+              <Avatar
+                src={
+                  post.User.avatar ||
+                  'https://source.unsplash.com/user/avatar/100x100'
+                }
+              />
+            }
+            title={<Title level={3}>{post.title}</Title>}
+            description={
+              <Text>
+                {post.content.length > 150
+                  ? `${post.content.slice(0, 150)}...`
+                  : post.content}
+              </Text>
+            }
+          />
+          <Divider orientation="left">Tags</Divider>
+          {post.Hashtags.map((tag) => (
+            <Link href={`/tag/${tag.name}`}>
+              <Tag> {tag.name}</Tag>
+            </Link>
+          ))}
+        </Card>
+      </Link>
+    </Col>
+  </>
+);
 
 export default MainPostCard;
