777a745be95131a7112fa292ba3df8a610e94b68
Aio <AioDev.JS@gmail.com>
2021-05-27 20:33:09 +0900

    #2 무한 스크롤 작동만 가능

--- front/pages/index.js
+++ front/pages/index.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSWRInfinite } from 'swr';
 import Head from 'next/head';
-import { Row, Spin } from 'antd';
+import { Button, Row, Spin } from 'antd';
 import AppLayout from '../components/layouts/AppLayout';
 import { backUrl } from '../config/config';
 import { generateDummyPosts } from '../util/dummy';
 import MainPostCard from '../components/MainPostCard';
 import PostWriteButton from '../components/PostWriteButton';
 
-// const fetcher = (url) => axios.get(url, { withCredentials: true }).then((result) => result.data);
-// const fakeFecther = (url) => Promise.resolve(generateDummyPosts(12));
+const fetcher = (url) =>
+  axios.get(url, { withCredentials: true }).then((result) => {
+    console.log('fetcher', result);
+    return result.data;
+  });
 const fakeFecther = (url) =>
   new Promise((resolve) => {
     const data = generateDummyPosts(12);
@@ -18,18 +21,38 @@ const fakeFecther = (url) =>
   });
 const getKey = (pageIndex, previousPageData) => {
   if (previousPageData && !previousPageData.length) return null; // reached the end
-  return `/posts?page=${pageIndex}&limit=12`; // SWR key
+  // return `/posts?lastId=${pageIndex}&limit=12`; // SWR key
+  return `http://localhost:3005/posts?lastId=12?index=${pageIndex}`; // SWR key
 };
 
 const Home = () => {
-  const [postsLoadLimit, setPostsLoadLimit] = useState(10);
+  const [postsLoadLimit, setPostsLoadLimit] = useState(12);
 
   const {
     data: postsDataArray,
     error: postsError,
     size,
     setSize,
-  } = useSWRInfinite(getKey, fakeFecther);
+  } = useSWRInfinite(
+    (pageIndex, previousPageData) => {
+      console.log('pageindex:', pageIndex);
+      console.log('이전 데이터', previousPageData);
+      if (previousPageData && !previousPageData.length) return null; // reached the end
+      // return `/posts?lastId=${pageIndex}&limit=12`; // SWR key
+      let lastId = null;
+      if (previousPageData) {
+        lastId = previousPageData[previousPageData.length - 1].id;
+      }
+      return `http://localhost:3005/posts?${
+        lastId ? `lastId=${lastId}?` : ''
+      }indx=${pageIndex + 1}`; // SWR key
+    },
+    fetcher,
+    {
+      // focusThrottleInterval: 3000,
+      // dedupingInterval: 3000,
+    },
+  );
 
   const hasMorePosts = true;
   useEffect(() => {
@@ -39,6 +62,7 @@ const Home = () => {
         document.documentElement.scrollHeight - 100
       ) {
         if (postsDataArray && postsDataArray.length === size) {
+          console.log('온 스크롤 동작');
           setSize(size + 1);
         }
       }
@@ -48,6 +72,10 @@ const Home = () => {
       window.removeEventListener('scroll', onScroll);
     };
   }, [postsDataArray]);
+
+  const loadMorePosts = () => {
+    setSize(size + 1);
+  };
 
   console.log(postsDataArray);
   return (
@@ -60,11 +88,16 @@ const Home = () => {
         {!postsDataArray ? (
           <Spin size="large" />
         ) : (
-          <Row gutter={8} align="middle">
-            {postsDataArray.map((arr) =>
-              arr.map((post) => <MainPostCard post={post} />),
-            )}
-          </Row>
+          <>
+            <Row gutter={8} align="middle">
+              {postsDataArray.map((arr) =>
+                arr.map((post) => <MainPostCard post={post} />),
+              )}
+            </Row>
+            <Button type="primary" onClick={loadMorePosts}>
+              불러오기
+            </Button>
+          </>
         )}
       </AppLayout>
     </>
