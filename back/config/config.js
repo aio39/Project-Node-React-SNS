86f17e704387bba24b21db98652521a2c675fa32
Aio <AioDev.JS@gmail.com>
2021-05-28 11:31:30 +0900

    #6 sequelize config - 클라우드 DB 연결

--- back/config/config.js
+++ back/config/config.js
@@ -5,21 +5,22 @@ dotenv.config();
 module.exports = {
   development: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: process.env.MYSQL_DEV_PASSWORD,
     database: 'node-project',
-    host: '127.0.0.1',
+    host: process.env.MYSQL_DEV_HOST,
+    port: process.env.MYSQL_DEV_PORT,
     dialect: 'mysql',
   },
   test: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: process.env.MYSQL_DB_PASSWORD,
     database: 'node-project',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: process.env.MYSQL_DB_PASSWORD,
     database: 'node-project',
     host: '127.0.0.1',
     dialect: 'mysql',
