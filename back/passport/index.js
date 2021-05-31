de80438fe1ea5bf9ced80a9f83f00e16aae26c88
Aio <AioDev.JS@gmail.com>
2021-05-31 16:44:09 +0900

    #7 passport 주석 추가

--- back/passport/index.js
+++ back/passport/index.js
@@ -3,10 +3,12 @@ const local = require('./local');
 const { User } = require('../models');
 
 module.exports = () => {
+  //  로그인에 성공하면 serializeUser 유저를 실행하며 세션 key를 만듬
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
+  //  passport.session() 이  req.session의 데이터를 분석해서 req.user로 넣어줌.
   passport.deserializeUser(async (id, done) => {
     try {
       const user = await User.findOne({
