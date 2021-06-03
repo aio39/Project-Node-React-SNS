9f44beb5aa6c7e16b6412cbd90acb75a3773e33c
Aio <AioDev.JS@gmail.com>
2021-06-03 12:28:05 +0900

    #6 테스트용 테스트 코드 추가

--- back/routes/debug.js
+++ back/routes/debug.js
@@ -1,6 +1,54 @@
 const express = require('express');
+const { Op } = require('sequelize');
+const { Comment, sequelize, Post, Hashtag } = require('../models');
 
 const router = express.Router();
+
+router.get('/groups', async (req, res) => {
+  const result = await Comment.findAll({
+    attributes: [
+      'PostId',
+      [sequelize.fn('COUNT', sequelize.col('PostId')), 'count'],
+    ],
+    group: ['Comment.PostId'],
+  });
+
+  res.json(result);
+});
+
+router.get('/tagsearch', async (req, res) => {
+  console.log(req.query.tag);
+  const result = await Hashtag.findAll({
+    attributes: ['id', 'name'],
+    where: {
+      name: {
+        [Op.like]: `%${decodeURI(req.query.tag)}%`,
+      },
+    },
+  });
+
+  const map = await Promise.all(
+    result.map(async tag => {
+      const a = 0;
+      return 'OK';
+    }),
+  );
+  console.log(result);
+  res.json(result);
+});
+
+router.get('/count', async (req, res) => {
+  const result = await Post.findAll({
+    attributes: [
+      'Post.*',
+      'comments.*',
+      [sequelize.fn('COUNT', sequelize.col('comments.id')), 'count'],
+    ],
+    include: [Comment],
+  });
+
+  res.json(result);
+});
 
 router.all('/', (req, res) => {
   res.json({
