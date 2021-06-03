73889dea29a1787ed00bca632a179423f688701b
Aio <AioDev.JS@gmail.com>
2021-06-03 20:17:43 +0900

    #5 마이페이지 이름 변경 기능

--- front/pages/mypage.js
+++ front/pages/mypage.js
@@ -1,13 +1,19 @@
 import { FormOutlined, UploadOutlined, UserOutlined } from '@ant-design/icons';
-import { Avatar, Button, Col, Divider, Input, Row, Upload } from 'antd';
+import { Affix, Avatar, Button, Col, Divider, Input, Row, Upload } from 'antd';
 import Text from 'antd/lib/typography/Text';
 import Title from 'antd/lib/typography/Title';
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import Form from 'antd/lib/form/Form';
+import axios from 'axios';
 import AppLayout from '../components/layouts/AppLayout';
 import PostWriteButton from '../components/PostWriteButton';
 import { generateDummyMyData } from '../util/dummy';
 import fetcher from '../util/fetcher';
+
+import { loginValidation } from '../util/validation/yup';
 
 const fakeFecther = url =>
   new Promise(resolve => {
@@ -18,6 +24,9 @@ const fakeFecther = url =>
 const MyPage = () => {
   const { data: userData, revalidate } = useSWR('/user', fetcher);
 
+  const [isLoadingPostSingUp, setIsLoadingPatch] = useState(false);
+  const [isFailedPost, setIsFailedPatch] = useState(false);
+
   const uploadOnchange = ({ file }) => {
     if (file.status === 'done') {
       console.log('업로드!');
@@ -27,7 +36,39 @@ const MyPage = () => {
   console.log(userData);
   if (!userData) return null;
 
-  const handleNameChange = () => {};
+  const {
+    control,
+    // formState: { errors },
+    handleSubmit,
+  } = useForm({
+    // resolver: yupResolver(loginValidation),
+    mode: 'onBlur',
+  });
+
+  const onSubmit = handleSubmit(async data => {
+    console.log(data);
+    // console.log(errors);
+    setIsLoadingPatch(true);
+    setIsFailedPatch(false);
+    const result = await axios.patch('/user', data);
+    setIsLoadingPatch(false);
+    console.log(result);
+    if (result.statusText === 'OK') {
+      console.log('성공', result);
+      const res = await revalidate();
+      console.log(res);
+    } else {
+      console.log('실패', result);
+      setIsFailedPatch(true);
+    }
+  });
+
+  const [isEditingName, setIsEditingName] = useState(false);
+
+  const handleNameChange = () => {
+    setIsEditingName(p => !p);
+    return null;
+  };
 
   return (
     <>
@@ -64,25 +105,49 @@ const MyPage = () => {
               <Button icon={<UploadOutlined />} />
             </Upload>
           </Col>
-          <Col xl={8} sm={24}>
-            <Input
-              prefix={
-                <>
-                  <Text>이름</Text> <Divider type="vertical" />
-                </>
-              }
-              suffix={
-                <Button
-                  shape="circle"
-                  onClick={handleNameChange}
-                  loading={false}
-                  icon={<FormOutlined />}
-                />
-              }
-              disabled={false}
-              defaultValue={userData.nickname}
-            />
-          </Col>
+          <Form onFinish={onSubmit} size="large">
+            <Col xl={8} sm={24}>
+              <Controller
+                name="nickname"
+                type="text"
+                control={control}
+                defaultValue={userData.nickname}
+                render={({ field }) => (
+                  <Input
+                    disabled={!isEditingName}
+                    prefix={
+                      <>
+                        <Text>이름</Text> <Divider type="vertical" />
+                      </>
+                    }
+                    suffix={
+                      <Button
+                        shape="circle"
+                        onClick={handleNameChange}
+                        icon={<FormOutlined />}
+                      />
+                    }
+                    {...field}
+                  />
+                )}
+              />
+
+              {isEditingName ? (
+                <Affix
+                  target={() => window}
+                  style={{ position: 'fixed', bottom: '10px' }}
+                >
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    loading={isLoadingPostSingUp}
+                  >
+                    SAVE
+                  </Button>
+                </Affix>
+              ) : null}
+            </Col>
+          </Form>
         </Row>
         <Row>
           <Col>
