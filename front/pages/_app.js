a9d7e812de46a94046a07b774b4f2e1cb799e870
Aio <AioDev.JS@gmail.com>
2021-06-03 17:32:19 +0900

    #12 swr-devtool

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
-
+import SWRDevtools from '@jjordy/swr-devtools';
+import { cache, mutate } from 'swr';
 import axios from 'axios';
 import wrapper from '../store/configureStore';
 import backUrl from '../config/axios_config';
@@ -12,6 +13,7 @@ axios.defaults.withCredentials = true;
 
 const App = ({ Component }) => (
   <>
+    <SWRDevtools cache={cache} mutate={mutate} />
     <Head>
       <meta charSet="utf-8" />
       <title>App</title>
