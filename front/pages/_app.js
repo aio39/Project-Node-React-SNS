3e850ef005aa2301e5d09ff755242ddab0e41376
Aio <AioDev.JS@gmail.com>
2021-06-10 19:20:14 +0900

    #1 Progessbar 추가

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -5,6 +5,7 @@ import 'antd/dist/antd.css';
 import SWRDevtools from '@jjordy/swr-devtools';
 import { cache, mutate } from 'swr';
 import axios from 'axios';
+import NextNprogress from 'nextjs-progressbar';
 import wrapper from '../store/configureStore';
 import backUrl from '../config/axios_config';
 
@@ -14,6 +15,13 @@ axios.defaults.withCredentials = true;
 const App = ({ Component }) => (
   <>
     <SWRDevtools cache={cache} mutate={mutate} />
+    <NextNprogress
+      color="#1890FF"
+      startPosition={0.2}
+      stopDelayMs={200}
+      height={6}
+      showOnShallow
+    />
     <Head>
       <meta charSet="utf-8" />
       <title>App</title>
