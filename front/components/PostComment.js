93f0c430ffd390bc59c17b56448c5c0186133ebf
Aio <AioDev.JS@gmail.com>
2021-06-10 20:31:38 +0900

    #8 코멘트 부분 코드리팩토링

--- front/components/PostComment.js
+++ front/components/PostComment.js
@@ -1,10 +1,17 @@
 import { Avatar, Comment } from 'antd';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import Axios from 'axios';
 import styled from 'styled-components';
 import CommentTextArea from './CommentTextArea';
 import DeleteBtn from './button/DeleteBtn';
+
+const DeleteBtnWrapper = styled('div')`
+  position: absolute;
+  top: 20px;
+  right: 10px;
+  color: red;
+`;
 
 const PostComment = ({ comment, loginUserId }) => {
   const { User, Reply, PostId, id: CommentId, isDeleted } = comment;
@@ -12,6 +19,17 @@ const PostComment = ({ comment, loginUserId }) => {
   const onClickReply = useCallback(() => {
     setReplyTurnOn(!replyTurnOn);
   }, []);
+
+  const requestObj = useMemo(
+    () => ({
+      target: '덧글',
+      kind: 'comment',
+      postId: PostId,
+      commentId: CommentId,
+    }),
+    [PostId, CommentId],
+  );
+
   return (
     <Comment
       actions={[
@@ -26,23 +44,9 @@ const PostComment = ({ comment, loginUserId }) => {
       content={<p>{comment.content}</p>}
     >
       {User.id === loginUserId && !isDeleted ? (
-        <div
-          style={{
-            position: 'absolute',
-            top: '20px',
-            right: '10px',
-            color: 'red',
-          }}
-        >
-          <DeleteBtn
-            requestObj={{
-              target: '덧글',
-              kind: 'comment',
-              postId: PostId,
-              commentId: CommentId,
-            }}
-          />
-        </div>
+        <DeleteBtnWrapper>
+          <DeleteBtn requestObj={requestObj} />
+        </DeleteBtnWrapper>
       ) : null}
       {replyTurnOn ? (
         <CommentTextArea PostId={PostId} CommentId={CommentId} />
